1eaea98019a91ba8335196beefc2e6b29ad1a1bb
yohei.hokari <yohei.hokari@gmail.com>
2025-01-13 23:45:03 +0900

    railsニュースのファイル名を変更

---
+++ rails-notify-slack.sh
@@ -0,0 +1,14 @@
+LINKS_FILE="rails-urls.yml"
+
+message="Weekly News：\n"
+while IFS= read -r line; do
+  if [[ $line =~ ^[[:space:]]*([^:]+):[[:space:]]*(https?://.+)$ ]]; then
+    title="${BASH_REMATCH[1]}"
+    link="${BASH_REMATCH[2]}"
+    message+="- ${title}: ${link}\n"
+  fi
+done < "$LINKS_FILE"
+
+# Slackに通知
+curl -X POST -H 'Content-type: application/json' \
+  --data "{\"text\": \"$message\"}" "$SLACK_WEBHOOK_URL"
